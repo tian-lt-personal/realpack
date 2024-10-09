33c0527a7b66a31b167097f9b075970c45796864
Tian L <tankle_@hotmail.com>
2024-10-10 01:31:37 +0900

    init with signed

--- src/realpack/include/z.hpp
+++ src/realpack/include/z.hpp
@@ -21,6 +21,7 @@ template <class T>
 concept z_digit_container = std::ranges::contiguous_range<T> && std::ranges::sized_range<T> && requires {
   typename T::value_type;
   sizeof(typename T::value_type) <= sizeof(size_t);
+  std::is_unsigned_v<typename T::value_type>;
 };
 
 template <z_digit_container C = details::z_default_container, size_t B = details::z_default_base>
@@ -40,9 +41,14 @@ constexpr bool is_zero(const z<C, B>& num) noexcept {
   return std::ranges::all_of(num.digits, [](typename z<C, B>::digit_type x) { return x == 0; });
 }
 
-template <z_digit_container C, size_t B>
-constexpr z<C, B>& init_ui(z<C, B>& num, std::unsigned_integral auto val) {
-  num.sign = false;  // positive
+template <z_digit_container C, size_t B, std::integral T>
+constexpr z<C, B>& init_z(z<C, B>& num, T val) {
+  if constexpr (std::is_unsigned_v<T>) {
+    num.sign = false;
+  } else {
+    num.sign = val < 0;
+    val = num.sign ? -val : val;
+  }
   while (val > 0) {
     num.digits.push_back(val % B);
     val /= B;
--- src/tests/ut/n_tests.cpp
+++ src/tests/ut/n_tests.cpp
@@ -10,7 +10,7 @@ namespace n_tests_is_zero {
 static_assert(real::is_zero(real::z{}));
 static_assert([] {
   real::z num;
-  real::init_ui(num, 1u);
+  real::init_z(num, 1u);
   return !real::is_zero(num);
 }());
 
@@ -23,28 +23,28 @@ TEST(n_tests, is_zero) {
   }
   {
     real::z num;
-    real::init_ui(num, 1u);
+    real::init_z(num, 1u);
     EXPECT_FALSE(real::is_zero(num));
   }
 }
 
-TEST(n_tests, init_ui) {
+TEST(n_tests, init_z_unsigned) {
   {
     real::z<std::vector<unsigned>, 10> dec;
-    real::init_ui(dec, 0u);
+    real::init_z(dec, 0u);
     EXPECT_TRUE(dec.digits.empty());
     EXPECT_FALSE(dec.sign);
   }
   {
-    real::z<std::vector<unsigned>, 10> dec;
-    real::init_ui(dec, 1u);
+    real::z<std::vector<unsigned char>, 10> dec;
+    real::init_z(dec, 1u);
     EXPECT_FALSE(dec.sign);
     EXPECT_EQ(dec.digits.size(), 1);
     EXPECT_EQ(dec.digits[0], 1);
   }
   {
-    real::z<std::vector<unsigned>, 3> dec;
-    real::init_ui(dec, 5u);
+    real::z<std::vector<unsigned long long>, 3> dec;
+    real::init_z(dec, 5u);
     EXPECT_FALSE(dec.sign);
     EXPECT_EQ(dec.digits.size(), 2);
     EXPECT_EQ(dec.digits[0], 2);
@@ -52,8 +52,41 @@ TEST(n_tests, init_ui) {
   }
   {
     real::z<std::vector<unsigned>, 3> dec;
-    real::init_ui(dec, 6u);
+    real::init_z(dec, 6u);
     EXPECT_FALSE(dec.sign);
+    EXPECT_EQ(dec.digits.size(), 2);
+    EXPECT_EQ(dec.digits[0], 0);
+    EXPECT_EQ(dec.digits[1], 2);
+  }
+}
+
+TEST(n_tests, init_z_signed) {
+  {
+    real::z<std::vector<unsigned>, 10> dec;
+    real::init_z(dec, 0);
+    EXPECT_TRUE(dec.digits.empty());
+    EXPECT_FALSE(dec.sign);
+  }
+  {
+    real::z<std::vector<unsigned>, 10> dec;
+    real::init_z(dec, -1);
+    EXPECT_FALSE(dec.digits.empty());
+    EXPECT_TRUE(dec.sign);
+    EXPECT_EQ(dec.digits.size(), 1);
+    EXPECT_EQ(dec.digits[0], 1);
+  }
+  {
+    real::z<std::vector<unsigned long long>, 3> dec;
+    real::init_z(dec, -5);
+    EXPECT_TRUE(dec.sign);
+    EXPECT_EQ(dec.digits.size(), 2);
+    EXPECT_EQ(dec.digits[0], 2);
+    EXPECT_EQ(dec.digits[1], 1);
+  }
+  {
+    real::z<std::vector<unsigned>, 3> dec;
+    real::init_z(dec, -6);
+    EXPECT_TRUE(dec.sign);
     EXPECT_EQ(dec.digits.size(), 2);
     EXPECT_EQ(dec.digits[0], 0);
     EXPECT_EQ(dec.digits[1], 2);
