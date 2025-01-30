e47dc230241d48db71e197ff3626bdbcea59ddad
Tian L <tankle_@hotmail.com>
2025-01-31 00:51:06 +0900

    handle ws

--- src/libmath/specs/tokenizer.re
+++ src/libmath/specs/tokenizer.re
@@ -1,4 +1,5 @@
 #include <ios>
+
 #include "parse.hpp"
 
 namespace real::math::parse {
@@ -6,6 +7,7 @@ namespace real::math::parse {
 
   std::expected<token, token_error> tokenizer::operator()() {
     std::streampos t1, t2, t3; // temporary states for s-tags
+    for(;;) {
     try {
 /*!stags:re2c format = 'std::streampos @@;\n'; */
 /*!re2c
@@ -21,17 +23,24 @@ namespace real::math::parse {
   re2c:yyfill:enable = 0;
   re2c:tags = 1;
 
+  ws = [\t\v\b\f\n\r ]*;
   id = [a-zA-Z_][a-zA-Z0-9_]*;
 
   * { return std::unexpected{token_error{.code = token_error_code::bad_token, .what = std::nullopt}}; }
+  ws { continue; }
   @t1 id {
      return token{.type = token_type::id, .str = get_string(in_, t1)};
   }
 */
-    }
+    } // end of try
     catch(const std::ios_base::failure& ex) {
-      return std::unexpected{token_error{.code = token_error_code::stream_error, .what = ex.what()}};
+      if (in_.eof()) {
+        return std::unexpected{token_error{.code = token_error_code::eof, .what = std::nullopt}};
+      } else {
+        return std::unexpected{token_error{.code = token_error_code::stream_error, .what = ex.what()}};
+      }
     }
-  }
+  } // end of for
+  } // end of operator()
   
 }  // namespace real::math::parse
--- src/tests/ut/tokenizer_tests.cpp
+++ src/tests/ut/tokenizer_tests.cpp
@@ -18,6 +18,44 @@ std::istringstream create_stream(std::string text) {
 }
 }  // namespace
 
+TEST(tokenizer_tests, empty) {
+  {
+    auto stream = create_stream("");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream("\t");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream("  \n");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream("  \r\n");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream("\r\n\r\n\v\f");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+}
+
 TEST(tokenizer_tests, ids) {
   {
     auto stream = create_stream("abc");
@@ -27,5 +65,50 @@ TEST(tokenizer_tests, ids) {
     EXPECT_EQ(t->type, parse::token_type::id);
     EXPECT_TRUE(t->str.has_value());
     EXPECT_EQ(*t->str, "abc");
+  }
+}
+
+TEST(tokenizer_tests, whitespaces) {
+  {
+    auto stream = create_stream("   ");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream(" \t\nabc");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_TRUE(t.has_value());
+    EXPECT_EQ(t->type, parse::token_type::id);
+    EXPECT_TRUE(t->str.has_value());
+    EXPECT_EQ(*t->str, "abc");
+  }
+  {
+    auto stream = create_stream("\v\fabc  ");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_TRUE(t.has_value());
+    EXPECT_EQ(t->type, parse::token_type::id);
+    EXPECT_TRUE(t->str.has_value());
+    EXPECT_EQ(*t->str, "abc");
+    t = get_token();
+    EXPECT_FALSE(t.has_value());
+    EXPECT_EQ(t.error().code, parse::token_error_code::eof);
+  }
+  {
+    auto stream = create_stream("\v\fabc  \n\tdef");
+    parse::tokenizer get_token{stream};
+    auto t = get_token();
+    EXPECT_TRUE(t.has_value());
+    EXPECT_EQ(t->type, parse::token_type::id);
+    EXPECT_TRUE(t->str.has_value());
+    EXPECT_EQ(*t->str, "abc");
+    t = get_token();
+    EXPECT_TRUE(t.has_value());
+    EXPECT_EQ(t->type, parse::token_type::id);
+    EXPECT_TRUE(t->str.has_value());
+    EXPECT_EQ(*t->str, "def");
   }
 }
